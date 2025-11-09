9efb45757dba28ef5be892665984d9ef2181c7c7
Abdul Mozid <mozidkhan2020@gmail.com>
2025-11-09 12:02:51 +0600

    make api

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://Online-Learning-Platform-db:LtJ549iPMjmI8s3n@project-1.3gjxivd.mongodb.net/?appName=Project-1";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("Online-Learning-Platform-db");
+    const coursesCollection = db.collection("Courses");
+
+    app.get("/courses", async (req, res) => {
+      const result = await coursesCollection.find().toArray();
+
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
