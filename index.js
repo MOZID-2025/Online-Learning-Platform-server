3d6eb499760fc88651f23aa6b285379c1e96ec93
Abdul Mozid <mozidkhan2020@gmail.com>
2025-11-13 12:29:53 +0600

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -26,10 +26,77 @@ async function run() {
     const db = client.db("Online-Learning-Platform-db");
     const coursesCollection = db.collection("Courses");
 
+    //find
+    //findOne
     app.get("/courses", async (req, res) => {
       const result = await coursesCollection.find().toArray();
-
       res.send(result);
+    });
+
+    app.get("/courses/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await coursesCollection.findOne({ _id: new ObjectId(id) });
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    //post method
+    //insertOne
+    //insertMany
+
+    app.post("/courses", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await coursesCollection.insertOne(data);
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    //PUT
+    //UpdateOne
+    //UpdateMany
+
+    app.put("/courses/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+      const update = {
+        $set: data,
+      };
+      const result = await coursesCollection.updateOne(filter, update);
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    app.delete("/courses/:id", async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+      const result = await coursesCollection.deleteOne(filter);
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    //features 6 course
+    //get
+    //find
+
+    app.get("/features-course", async (req, res) => {
+      const result = await coursesCollection.find().sort().limit(6).toArray();
+      res.send(result);
+      console.log(result);
     });
 
     await client.db("admin").command({ ping: 1 });
